38e1179d051d70c3acf11378619ef3a004a1d696
Matthias Koch <ithrowexceptions@gmail.com>
2021-09-06 19:13:49 +0200

    wip

---
+++ source/Nuke.MSBuildTasks/InstallNpmToolsTask.cs
@@ -0,0 +1,45 @@
+﻿// Copyright 2021 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using System;
+using System.Linq;
+using JetBrains.Annotations;
+using Microsoft.Build.Framework;
+using NuGet.Versioning;
+using Nuke.Common;
+using Nuke.Common.Tools.Npm;
+
+namespace Nuke.MSBuildTasks
+{
+    [UsedImplicitly]
+    public class InstallNpmToolsTask : ContextAwareTask
+    {
+        [Microsoft.Build.Framework.Required]
+        public ITaskItem[] NpmTools { get; set; }
+
+        protected override bool ExecuteInner()
+        {
+            var installedNpmTools = NpmTasks.Npm("list -g")
+                .Select(x => x.Text.Split(' '))
+                .Where(x => x.Length == 2)
+                .Select(x => x.Last())
+                .ToDictionary(
+                    x => x.Substring(startIndex: 0, x.LastIndexOf("@", StringComparison.Ordinal)),
+                    x => NuGetVersion.Parse(x.Substring(x.LastIndexOf("@", StringComparison.Ordinal) + 1)));
+
+            foreach (var tool in NpmTools)
+            {
+                var package = tool.ItemSpec;
+                var version = tool.GetMetadata("Version").NotNull("version != null");
+
+                if (installedNpmTools.GetValueOrDefault(package)?.ToString() == version)
+                    continue;
+
+                NpmTasks.Npm($"install {package}@{version} -g");
+            }
+
+            return true;
+        }
+    }
+}
--- source/Nuke.SourceGenerators/Shims/DictionaryExtensions.cs
+++ source/Nuke.SourceGenerators/Shims/DictionaryExtensions.cs
@@ -19,5 +19,14 @@ namespace Nuke.Common.Utilities.Collections
         {
             return dictionary.TryGetValue(key, out var value) ? value : defaultValue;
         }
+
+        [CanBeNull]
+        public static TValue GetValueOrDefault<TKey, TValue>(
+            this IDictionary<TKey, TValue> dictionary,
+            TKey key,
+            TValue defaultValue = default)
+        {
+            return dictionary.TryGetValue(key, out var value) ? value : defaultValue;
+        }
     }
 }
